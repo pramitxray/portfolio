bf0e6f4a7be94232efb4e43205a32e493a4b70d7
Pramit Ray <91790123+pramitxray@users.noreply.github.com>
2024-08-04 22:32:02 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export default function Footer() {
-  return (
-    // <footer className="mb-10 px-4 text-center text-gray-500">
-    //   <p className="text-xs">
-    //     <span className="font-semibold">Built with React & Next.js<span>.
-    //   </p>
-    // </footer>
-  );
+    return (
+        <footer className="mb-10 px-4 text-center text-gray-500">
+            <p className="text-xs">
+                <span className="font-semibold">Built with React & Next.js</span>
+            </p>
+        </footer>
+    );
 }
