77b81cd35ee16651e8bab8b6d52ec3544f37807d
Pramit Ray <91790123+pramitxray@users.noreply.github.com>
2024-08-04 22:13:53 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, Resend API, Vercel hosting.
+        <span className="font-semibold">Built with React & Next.js<span> 
       </p>
     </footer>
   );
